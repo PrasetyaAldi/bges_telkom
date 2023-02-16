b83723a67295c929f05988b37af38346f9e11aa6
Rizaldi <prasetyaaldi098@gmail.com>
2023-02-16 09:21:45 +0700

    (style) add icon download

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -208,7 +208,8 @@
                         </div>
                         <div>
                             <button type="button" wire:click="converToPDF"
-                                class="inline-block px-8 py-2 m-0 text-xs font-bold text-center text-white uppercase align-middle transition-all border-0 rounded-lg cursor-pointer ease-soft-in leading-pro tracking-tight-soft bg-gradient-fuchsia shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85">pdf</button>
+                                class="inline-block px-8 py-2 m-0 text-xs font-bold text-center text-white uppercase align-middle transition-all border-0 rounded-lg cursor-pointer ease-soft-in leading-pro tracking-tight-soft bg-gradient-fuchsia shadow-soft-md bg-150 bg-x-25 hover:scale-102 active:opacity-85"><i
+                                    class="fa fa-download"></i> pdf</button>
                         </div>
                     </div>
                 </div>
